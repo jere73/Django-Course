95f84f2c6b13bdd5f57397b2d667c95d354496c8
jere73 <jereosuna73@gmail.com>
2020-01-22 16:20:57 -0300

    Borrado.

--- platzigram/urls.py
+++ platzigram/urls.py
@@ -5,9 +5,5 @@ from platzigram import views as local_views
 from posts import  views as posts_views
 
 urlpatterns = [
-    path('hello-world/', local_views.hello_world),
-    path('sorted/', local_views.sorted),
-    path('hi/<str:name>/<int:age>/', local_views.say_hi),
-
-    path('posts/', posts_views.lists_posts)
+    path('admin/', admin.site.urls),
 ]
